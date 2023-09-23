d29b9019698b8cff4b792b61e0bec807f5074e10
ronald116 <ronaldbotchway@gmail.com>
2023-09-23 11:11:05 +0000

    updated basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -23,23 +23,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            # from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            # storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                setattr(self, key, value)
-                if type(self.created_at) is str:
-                    self.created_at = datetime.strptime(self.created_at,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-                if type(self.updated_at) is str:
-                    self.updated_at = datetime.strptime(self.updated_at,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        for key, value in kwargs.items():
+            if key == '__class__':
+                continue
+            setattr(self, key, value)
+            if type(self.created_at) is str:
+                self.created_at = datetime.strptime(self.created_at,
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            if type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(self.updated_at,
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
 
     def __str__(self):
         """Returns a string representation of the instance"""
